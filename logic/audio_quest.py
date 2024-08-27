0e898ede51eb0297f5cf6934fa13b6c3650c599c
Danial Negibov <danial666@ukr.net>
2024-08-27 16:22:54 +0300

    screen

--- logic/audio_quest.py
+++ logic/audio_quest.py
@@ -127,3 +127,41 @@ class AudioQuestionsScreen(MDScreen):
 
         # Обновляем словарь с выбранным вариантом для этого вопроса
         self.selected_options[question_id] = option_id
+
+    def check_answers(self):
+        # Проверка True/False вопросов
+        for i in range(1, 6):
+            user_answer = self.selected_options.get(f't_f_q{i}', '')
+            correct_answer = getattr(self, f't_f_cor_ans_q{i}').replace('"', '')  # Убираем кавычки
+
+            print(f"Checking True/False Question {i}: user_answer={user_answer}, correct_answer={correct_answer}")
+
+            if user_answer:
+                true_option_id = f'true_option_{i}'
+                false_option_id = f'false_option_{i}'
+
+                if user_answer == correct_answer.lower():
+                    self.ids[f'{user_answer}_option_{i}'].md_bg_color = [0 / 255, 128 / 255, 0 / 255, 1]
+                else:
+                    correct_option_id = true_option_id if correct_answer.lower() == 'true' else false_option_id
+                    self.ids[f'{user_answer}_option_{i}'].md_bg_color = [255 / 255, 0 / 255, 0 / 255, 1]
+                    self.ids[correct_option_id].md_bg_color = [0 / 255, 128 / 255, 0 / 255, 1]
+
+        # Проверка Fill in the Blanks вопросов
+        for i in range(1, 6):
+            user_answer_id = self.selected_options.get(f'q{i}', '')
+            correct_answer = getattr(self, f'cor_ans_q{i}')
+
+            if user_answer_id:
+                # Получаем текст из MDLabel внутри MDCard
+                user_answer_text = self.ids[user_answer_id].children[0].text
+
+                if user_answer_text == correct_answer:
+                    self.ids[user_answer_id].md_bg_color = [0 / 255, 128 / 255, 0 / 255, 1]  # Зеленый цвет
+                else:
+                    self.ids[user_answer_id].md_bg_color = [255 / 255, 0 / 255, 0 / 255, 1]  # Красный цвет
+                    # Находим правильный ответ среди ID и меняем его цвет на зеленый
+                    for option_id in ['option_1_q' + str(i), 'option_2_q' + str(i), 'option_3_q' + str(i)]:
+                        if self.ids[option_id].children[0].text == correct_answer:
+                            self.ids[option_id].md_bg_color = [0 / 255, 128 / 255, 0 / 255, 1]  # Зеленый цвет
+                            break
