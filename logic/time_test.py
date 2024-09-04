e0ef245e9e4c858156956aa9b27dfde7c94f567d
Danial Negibov <danial666@ukr.net>
2024-09-04 09:24:22 +0300

    db

--- logic/time_test.py
+++ logic/time_test.py
@@ -1,6 +1,9 @@
-from kivy.properties import ObjectProperty
-from kivymd.uix.menu import MDDropdownMenu
+from kivy.properties import StringProperty
 from kivymd.uix.screen import MDScreen
 
 class TimeTest(MDScreen):
-    pass
+    selected_time = StringProperty('30 секунд')  # Свойство для хранения выбранного времени
+
+    def set_time_and_navigate(self, time):
+        self.selected_time = time  # Устанавливаем выбранное время
+        self.manager.current = 'time_test_questions'  # Переход на экран
--- logic/time_test_questions.py
+++ logic/time_test_questions.py
@@ -3,4 +3,7 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.screen import MDScreen
 
 class TimeTestQuestions(MDScreen):
-    pass
+    def on_pre_enter(self, *args):
+        time_test_screen = self.manager.get_screen('time_test')
+        selected_time = time_test_screen.selected_time  # Получаем выбранное время
+        print(f"Выбранное время: {selected_time}")
