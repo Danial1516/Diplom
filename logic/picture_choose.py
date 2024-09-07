384e67b59c3808dbc710dc96491bb7dbd7ce6d67
Danial Negibov <danial666@ukr.net>
2024-09-07 12:40:09 +0300

    db

--- logic/picture_choose.py
+++ logic/picture_choose.py
@@ -11,9 +11,28 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class PictureChooseScreen(MDScreen):
     category = StringProperty()
+    selected_card = None
+    correct_card = None
+    is_checked = False  # Флаг для блокировки выбора после проверки
 
     def on_pre_enter(self, *args):
         self.update_images()
+
+    def on_leave(self, *args):
+        # Сброс состояния при выходе с экрана
+        self.reset_screen()
+
+    def reset_screen(self):
+        # Сбрасываем все состояния
+        self.is_checked = False
+        self.selected_card = None
+        self.correct_card = None
+        self.ids['check_button'].children[0].text = "Перевірити"
+        self.ids['check_button'].md_bg_color = [255/255, 223/255, 186/255, 1]
+
+        # Сбрасываем цвет всех карточек
+        for card in ['card1', 'card2', 'card3', 'card4']:
+            self.ids[card].md_bg_color = [240/255, 240/255, 240/255, 1]
 
     def update_images(self):
         session = SessionLocal()
@@ -33,24 +52,22 @@ class PictureChooseScreen(MDScreen):
         correct_image = random.choice(selected_images)
         captions[correct_image.image_url] = correct_image.correct_caption
 
+        # Сохраняем правильную карточку
+        self.correct_card = correct_image.image_url
+
         # Для оставшихся изображений выбираем неправильные подписи
         for img in selected_images:
             if img != correct_image:
-                # Найти неправильные подписи для текущего изображения
                 wrong_captions = [img.wrong_caption1, img.wrong_caption2, img.wrong_caption3]
-                # Выбираем случайную неправильную подпись
                 random_wrong_caption = random.choice(wrong_captions)
                 captions[img.image_url] = random_wrong_caption
 
-        # Перемешиваем изображения и подписи
         shuffled_images = list(captions.keys())
         random.shuffle(shuffled_images)
 
-        # Устанавливаем пути к изображениям и подписи
         image_sources = [f'assets/images/{img_url}' for img_url in shuffled_images]
         shuffled_captions = [captions[img_url] for img_url in shuffled_images]
 
-        # Убедимся, что список подписей и источников изображений совпадают по длине
         if len(shuffled_captions) != len(image_sources):
             raise ValueError("Количество подписей не совпадает с количеством изображений")
 
@@ -60,12 +77,52 @@ class PictureChooseScreen(MDScreen):
         # Обновляем карточки
         for i, image_id in enumerate(image_ids):
             if i < len(image_sources):
-                # Обновляем источник изображения
                 self.ids.get(image_id).source = image_sources[i]
 
-        # Обновляем тексты на метках
         for i, caption in enumerate(shuffled_captions):
             if i < len(label_ids):
                 self.ids.get(label_ids[i]).text = caption
 
         session.close()
+
+    def select_card(self, card_id):
+        if self.is_checked:
+            return  # Блокируем выбор после проверки
+
+        if self.selected_card:
+            self.ids[self.selected_card].md_bg_color = [240/255, 240/255, 240/255, 1]
+        self.selected_card = card_id
+        self.ids[card_id].md_bg_color = [173/255, 216/255, 230/255, 1]
+
+    def check_selection(self):
+        if not self.selected_card:
+            return  # Если карточка не выбрана, ничего не делать
+
+        self.is_checked = True  # Блокируем выбор после проверки
+
+        selected_image_id = self.ids[self.selected_card].children[1].source.split('/')[-1]
+
+        if selected_image_id == self.correct_card:
+            self.ids[self.selected_card].md_bg_color = [0, 1, 0, 1]  # Зеленый цвет
+            self.ids['check_button'].children[0].text = "Далі"
+            self.ids['check_button'].md_bg_color = [0, 1, 0, 1]
+        else:
+            self.ids[self.selected_card].md_bg_color = [1, 0, 0, 1]  # Красный цвет
+            for card in ['card1', 'card2', 'card3', 'card4']:
+                if self.correct_card in self.ids[card].children[1].source:
+                    self.ids[card].md_bg_color = [0, 1, 0, 1]
+                    break
+            self.ids['check_button'].children[0].text = "Далі"
+            self.ids['check_button'].md_bg_color = [1, 0, 0, 1]
+
+    def next_round(self):
+        self.is_checked = False
+        self.selected_card = None
+        self.correct_card = None
+        self.ids['check_button'].children[0].text = "Перевірити"
+        self.ids['check_button'].md_bg_color = [255/255, 223/255, 186/255, 1]
+
+        for card in ['card1', 'card2', 'card3', 'card4']:
+            self.ids[card].md_bg_color = [240/255, 240/255, 240/255, 1]
+
+        self.update_images()
